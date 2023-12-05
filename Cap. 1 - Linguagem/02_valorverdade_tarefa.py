4984a20e30e18c5c62a008ae8df44bf1d3c995e4
Felipe Barreto <felipesantana.7@gmail.com>
2023-12-05 17:38:07 -0300

    Initial commit

---
+++ Cap. 1 - Linguagem/02_valorverdade_tarefa.py
@@ -0,0 +1,5 @@
+# TODO: Mostre o valor booleano de uma lista vazia
+x = []
+print(bool(x))
+
+# TODO: Mostre o valor booleano de um dicionario vazio
---
+++ Cap. 1 - Linguagem/04_templstr_tarefa.py
@@ -0,0 +1,24 @@
+# Demonstração das funções de string templates
+from string import Template
+
+def main():
+    # Formatação tradicional usando o método format()
+    frase = "Você está assistindo {0} com {1}".format(
+        "Python Avançado", "Jessica Temporal")
+    print(frase)
+
+    # TODO: Crie um template com placeholders
+    templ = Template("Você está fazendo o curso ${curso} na plataforma ${plataforma}")
+
+    # TODO: Use o método substitute passando argumentos nomeados
+    frase_2 = templ.substitute(
+        curso = "Python Avançado",
+        plataforma = "Linkedin Learning" 
+    )
+
+    print(frase_2)
+    # TODO: Use  o método substitute com um dicionário
+
+
+if __name__ == "__main__":
+    main()
